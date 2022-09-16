4e0697b27d87fb57764ae8992a6d5ad852db7fea
Cyprian kaku <cypriankorir@gmail.com>
2022-09-16 12:49:38 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_line - prints a straight line
  * @n:parameter
  * Return:returns nothing
  */
-void print_line(int n)
-*/
 void print_line(int n)
 {
 while (n-- > 0)
