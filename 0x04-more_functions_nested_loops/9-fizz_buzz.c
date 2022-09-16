198ecfe58a128dc780e7e8951c9bc32bbd083536
Cyprian kaku <cypriankorir@gmail.com>
2022-09-16 14:27:56 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - check the code alx School students.
+ *
+ * Return: void.
+ */
+
+int main(void)
+{
+
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+		        _putchar(32);
+		}
+	
+		i++;
+		}
+		_putchar(10);
+		return (0);
+	}
