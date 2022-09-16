f9968eca0959ab12a1fda8a9cf57ee7b192f0dd7
Cyprian kaku <cypriankorir@gmail.com>
2022-09-16 13:42:15 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,24 @@
 #include "main.h"
+
 /**
- * print_diagonal - prints a diagonal
- * @n:parameter
- * Return:returns nothing
+ * print_diagonal - a function that draws a diagonal line on the terminal.
+ * @n: An input integer
+ * Return: Always 0
  */
 void print_diagonal(int n)
 {
-int len, space;
-if (n > 0)
-{
-for (len = 0; len < n; len++)
-{
-for (space = 0; space < len; space++)
-_putchar(32);
+	int i = 0, j;
 
-_putchar(92);
-
-if (len == (n - 1))
-continue;
-_putchar(10);
-}
-}
-_putchar(10);
-}
-}
-_putchar(10);
+	if (n > 0)
+	{
+		for (; i < n; i++)
+		{
+			for(j = 0; j < i; j++)
+				_putchar('');
+			_putchar(92);
+			_putchar('\n');
+		        }
+	}
+	else
+		_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+void print_diagonal(int n);
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
