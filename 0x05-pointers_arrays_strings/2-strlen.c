858844d647689587339bc26223d8137c3364e76b
Cyprian kaku <cypriankorir@gmail.com>
2022-09-19 08:54:47 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 /**
- * _strlen - Returns length of string
- * @str: string
- * Return: returns length;
- */
+* _strlen - Returns length of string
+* @str: string
+* Return: returns length;
+*/
 int _strlen(char *str)
 {
-	int count, inc;
-       	inc = 0;
-	for (count = 0; str[count] != '\0'; count++)
-	inc++;
+int count, inc;
+inc = 0;
+for (count = 0; str[count] != '\0'; count++)
+inc++;
 
-	return (inc);
+return (inc);
 }
