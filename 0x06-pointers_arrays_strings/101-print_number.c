d63b3bbab1155e6226d182ece2b4f953b8ea9b54
Cyprian kaku <cypriankorir@gmail.com>
2022-09-22 14:10:51 -0700

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,7 +10,6 @@ void print_number(int n)
 	unsigned int m, d, count;
 	if (n < 0)
 	{
-
 		_putchar(45);
 		m = n * -1;
 	}
@@ -20,13 +19,11 @@ void print_number(int n)
 	}
 		d = m;
 		count = 1;
-	
 	while (d > 9)	
 	{
-		d /= 10;
-		count *=10;
+		d / = 10;
+		count * =10;
 	}
-	
 	for (; count >= 1; count /= 10)
 	{
 		_putchar(((m /count) % 10) + 48);
